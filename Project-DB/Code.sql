2450fc19c98960f4924281b913056b207a021bde
WafelkaDew <119042132+WafelkaDew@users.noreply.github.com>
2025-11-27 10:33:50 +0500

    0.2 6 Practice

--- Project-DB/Code.sql
+++ Project-DB/Code.sql
@@ -1,27 +1,48 @@
-CREATE TABLE Customers(
-    customers_id INT PRIMARY KEY,
-    full_name VARCHAR(255),
-    email VARCHAR(255)
+CREATE TABLE Students (
+    student_id SERIAL PRIMARY KEY, -- Уникальный идентификатор студента
+    full_name VARCHAR(255) NOT NULL, -- Полное имя студента
+    email VARCHAR(255) UNIQUE NOT NULL, -- Электронная почта студента (должна быть уникальной)
+    start_year INT -- Год поступления студента
 );
 
-CREATE TABLE Orders(
-    order_id INT PRIMARY KEY,
-    date_order DATE,
-    customers_id INT,
-    FOREIGN KEY (customers_id) REFERENCES Customers (customers_id)
+CREATE TABLE Courses (
+    course_id SERIAL PRIMARY KEY, -- Уникальный идентификатор курса
+    course_name VARCHAR(255) NOT NULL, -- Название курса
+    credits INT CHECK (credits > 0) -- Количество зачетных единиц курса (должно быть больше 0)
 );
 
-CREATE TABLE Products(
-    product_id INT PRIMARY KEY,
-    product_name VARCHAR(255),
-    price INT
+CREATE TABLE Enrollments (
+    student_id INT REFERENCES Students(student_id) ON DELETE CASCADE, -- Ссылка на ID студента (внешний ключ)
+    course_id INT REFERENCES Courses(course_id) ON DELETE CASCADE, -- Ссылка на ID курса (внешний ключ)
+    grade CHAR(1), -- Оценка студента за курс (например, 'A', 'B', 'C')
+    PRIMARY KEY (student_id, course_id)
 );
 
-CREATE TABLE Product_in_order(
-    order_id INT,
-    product_id INT,
-    quantity INT,
-    PRIMARY KEY (order_id, product_id),
-    FOREIGN KEY (order_id) REFERENCES Orders (order_id),
-    FOREIGN KEY (product_id) REFERENCES Products(product_id)
-);
+INSERT INTO Students VALUES 
+(DEFAULT, 'Алексей Смирнов', 'Al@example.com', 2021),
+(DEFAULT, 'Елена Кузнецова', 'El@example.com', 2022),
+(DEFAULT, 'Дмитрий Новиков', 'Dmit@example.com', 2021),
+(DEFAULT, 'Ольга Морозова', 'Ol@example.com', 2023);
+
+INSERT INTO Courses VALUES 
+(DEFAULT, 'Введение в программирование', 5),
+(DEFAULT,'Базы данных', 4),
+(DEFAULT,'Веб-технологии', 4);
+
+INSERT INTO Enrollments VALUES
+(1, 2, 'A'),
+(2, 2, 'A'),
+(3, 1, NULL),
+(3,2, NULL),
+(3,3, NULL);
+
+UPDATE students
+SET email = 'elena.kuznetsova@newmail.com'
+WHERE full_name = 'Елена Кузнецова';
+
+UPDATE enrollments
+SET grade = 'A'
+WHERE student_id = 3 and course_id = 1;
+
+DELETE FROM Students
+WHERE email = 'elena.kuznetsova@newmail.com';
