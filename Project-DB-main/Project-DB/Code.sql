1093a5cdb0d6e3f25c905fe54cc4e968c95f3396
WafelkaDew <119042132+WafelkaDew@users.noreply.github.com>
2025-12-17 20:18:54 +0500

    0.6 practice 8

---
+++ Project-DB-main/Project-DB/Code.sql
@@ -0,0 +1,69 @@
+CREATE TABLE Students (
+    student_id SERIAL PRIMARY KEY, -- Уникальный идентификатор студента
+    full_name VARCHAR(255) NOT NULL, -- Полное имя студента
+    email VARCHAR(255) UNIQUE NOT NULL, -- Электронная почта студента (должна быть уникальной)
+    start_year INT -- Год поступления студента
+);
+
+CREATE TABLE Courses (
+    course_id SERIAL PRIMARY KEY, -- Уникальный идентификатор курса
+    course_name VARCHAR(255) NOT NULL, -- Название курса
+    credits INT CHECK (credits > 0) -- Количество зачетных единиц курса (должно быть больше 0)
+);
+
+CREATE TABLE Enrollments (
+    student_id INT REFERENCES Students(student_id) ON DELETE CASCADE, -- Ссылка на ID студента (внешний ключ)
+    course_id INT REFERENCES Courses(course_id) ON DELETE CASCADE, -- Ссылка на ID курса (внешний ключ)
+    grade CHAR(1), -- Оценка студента за курс (например, 'A', 'B', 'C')
+    PRIMARY KEY (student_id, course_id)
+);
+
+INSERT INTO Students VALUES 
+(DEFAULT, 'Алексей Смирнов', 'Al@example.com', 2021),
+(DEFAULT, 'Елена Кузнецова', 'El@example.com', 2022),
+(DEFAULT, 'Дмитрий Новиков', 'Dmit@example.com', 2021),
+(DEFAULT, 'Ольга Морозова', 'Ol@example.com', 2023);
+
+INSERT INTO Courses VALUES 
+(DEFAULT, 'Введение в программирование', 5),
+(DEFAULT,'Базы данных', 4),
+(DEFAULT,'Веб-технологии', 4);
+
+INSERT INTO Enrollments VALUES
+(1, 2, 'A'),
+(2, 2, 'A'),
+(3, 1, NULL),
+(3,2, NULL),
+(3,3, NULL);
+
+UPDATE students
+SET email = 'elena.kuznetsova@newmail.com'
+WHERE full_name = 'Елена Кузнецова';
+
+UPDATE enrollments
+SET grade = 'A'
+WHERE student_id = 3 and course_id = 1;
+
+DELETE FROM Students
+WHERE email = 'elena.kuznetsova@newmail.com';
+
+
+SELECT * FROM Students;
+
+SELECT course_name, credits FROM Courses;
+
+SELECT * FROM students WHERE start_year = 2021;
+
+SELECT * FROM courses WHERE credits > 4;
+
+SELECT * FROM students WHERE email = 'elena.kuznetsova.com';
+
+SELECT * FROM students WHERE full_name LIKE 'Дмитрий%';
+
+SELECT * FROM enrollments WHERE grade IS NULL;
+
+SELECT * FROM courses ORDER BY course_name;
+
+SELECT * FROM students ORDER BY start_year, full_name ASC;
+
+SELECT * FROM students ORDER BY start_year DESC LIMIT 2;
